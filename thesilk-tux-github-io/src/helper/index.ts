688f646a4fcd479e407e2230db3f0b0e91b49d75
theSilk <thesilk@posteo.net>
2020-03-30 23:46:11 +0200

    add helper script to create overview.json

---
+++ thesilk-tux-github-io/src/helper/index.ts
@@ -0,0 +1,38 @@
+const glob = require('fast-glob');
+const fs = require('fs');
+
+interface IBlog {
+  caption: string;
+  createdAt: string;
+  filename: string;
+  id: number;
+  imageSrc: string;
+  imageUrl: string;
+  leadParagraph: string;
+  updatedAt: string;
+}
+
+async function main() {
+  const blogs: IBlog[] = [];
+  let files = await glob('src/assets/blog/*.json');
+  files = files.filter((item: string) => !item.endsWith('overview.json'))
+
+  for (const file of files) {
+    const data = await fs.readFileSync(file);
+    const obj = JSON.parse(data);
+    blogs.push({
+      caption: obj.content.filter((item) => item.caption)[0].caption,
+      createdAt: obj.createdAt,
+      filename: file.replace('src/', ''),
+      id: obj.id,
+      imageSrc: obj.imgSrc,
+      imageUrl: obj.imgUrl,
+      leadParagraph: obj.content.filter((item) => item.lead)[0].lead,
+      updatedAt: obj.updatedAt,
+    });
+  }
+
+  fs.writeFileSync('src/assets/blog/overview.json', JSON.stringify(blogs, null, 4));
+}
+
+main();
