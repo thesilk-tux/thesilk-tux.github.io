226c02e6adad24e9c7a8e9b23f1d777c75de1b43
theSilk <thesilk@posteo.net>
2020-04-05 00:00:21 +0200

    fix unit tests (imports/declarations)

--- thesilk-tux-github-io/src/app/app.component.spec.ts
+++ thesilk-tux-github-io/src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -9,7 +11,9 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        ProfileComponent
       ],
     }).compileComponents();
   }));
@@ -24,12 +28,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('thesilk-tux-github-io');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('thesilk-tux-github-io app is running!');
   });
 });
--- thesilk-tux-github-io/src/app/blog/blog-entry/blog-entry.component.spec.ts
+++ thesilk-tux-github-io/src/app/blog/blog-entry/blog-entry.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { BlogEntryComponent } from './blog-entry.component';
+import { RouterModule } from '@angular/router';
 
 describe('BlogEntryComponent', () => {
   let component: BlogEntryComponent;
@@ -8,9 +10,9 @@ describe('BlogEntryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BlogEntryComponent ]
-    })
-    .compileComponents();
+      declarations: [BlogEntryComponent],
+      imports: [HttpClientTestingModule, RouterModule.forRoot([])],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
