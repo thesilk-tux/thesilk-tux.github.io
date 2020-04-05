fa947cc9f497262299c11c6bbff4a929137ef295
theSilk <thesilk@posteo.net>
2020-04-05 23:37:44 +0200

    improve covid-19 serivce
    fetch confirmed and deaths + calc their relative values

--- thesilk-tux-github-io/src/app/dashboard/covid.service.spec.ts
+++ thesilk-tux-github-io/src/app/dashboard/covid.service.spec.ts
@@ -20,19 +20,57 @@ describe('CovidService', () => {
   it('should extract country data as map', () => {
     const expected = new Map();
     expected.set('Australia', [
-      { date: '1/22/20', confirmed: 3 },
-      { date: '1/23/20', confirmed: 5 },
-      { date: '1/24/20', confirmed: 7 },
+      {
+        date: '1/22/20',
+        confirmed: 3,
+        deaths: 0,
+        relConfirmed: 0,
+        relDeaths: 0,
+      },
+      {
+        date: '1/23/20',
+        confirmed: 5,
+        deaths: 2,
+        relConfirmed: 0.4,
+        relDeaths: 1,
+      },
+      {
+        date: '1/24/20',
+        confirmed: 7,
+        deaths: 4,
+        relConfirmed: 0.2857,
+        relDeaths: 0.5,
+      },
     ]);
     expected.set('Germany', [
-      { date: '1/22/20', confirmed: 5 },
-      { date: '1/23/20', confirmed: 6 },
-      { date: '1/24/20', confirmed: 7 },
+      {
+        date: '1/22/20',
+        confirmed: 5,
+        deaths: 2,
+        relConfirmed: 0,
+        relDeaths: 0,
+      },
+      {
+        date: '1/23/20',
+        confirmed: 6,
+        deaths: 2,
+        relConfirmed: 0.1667,
+        relDeaths: 0,
+      },
+      {
+        date: '1/24/20',
+        confirmed: 7,
+        deaths: 4,
+        relConfirmed: 0.1429,
+        relDeaths: 0.5,
+      },
     ]);
 
-    const data =
+    const dataConfirmed =
       'Province/State,Country/Region,Lat,Long,1/22/20,1/23/20,1/24/20\nProvence 1,Australia,x,y,2,3,4\nProvence 2,Australia,x,y,1,2,3\n ,Germany,x,y,5,6,7';
-    const covidMap = service.getCovidData(data);
+    const dataDeaths =
+      'Province/State,Country/Region,Lat,Long,1/22/20,1/23/20,1/24/20\nProvence 1,Australia,x,y,0,2,2\nProvence 2,Australia,x,y,0,0,2\n ,Germany,x,y,2,2,4';
+    const covidMap = service.getCovidData(dataConfirmed, dataDeaths);
     expect(covidMap).toEqual(expected);
   });
 });
--- thesilk-tux-github-io/src/app/dashboard/covid.service.ts
+++ thesilk-tux-github-io/src/app/dashboard/covid.service.ts
@@ -2,66 +2,130 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 
+interface ICovidData {
+  date: string;
+  confirmed: number;
+  relConfirmed: number;
+  deaths: number;
+  relDeaths: number;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class CovidService {
-  private url =
-    'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
+  private urlGithubRaw =
+    'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/';
 
   constructor(private http: HttpClient) {}
 
-  public getCovidData(data: string): any {
-    let date: string[] = data.split('\n')[0].split(',');
+  public getCovidData(dataConfirmed: string, dataDeaths: string): any {
+    let date: string[] = dataConfirmed.split('\n')[0].split(',');
     date = date.slice(4, date.length);
 
-    let confirmeRaw: string[];
-    const confirmed = new Map();
-    let countryData: any[] = [];
+    const countryConfirmedRaw: string[] = dataConfirmed.split('\n');
+    countryConfirmedRaw.shift();
+    const countryDeathsRaw: string[] = dataDeaths.split('\n');
+    countryDeathsRaw.shift();
 
-    for (const country of data.split('\n').slice(1, data.length)) {
+    const covidData = new Map();
+    let countryData: ICovidData[] = [];
+
+    for (let idx = 0; idx < countryConfirmedRaw.length; idx++) {
       countryData = [];
-      confirmeRaw = country.split(',');
-
-      for (const [i, v] of confirmeRaw
-        .slice(4, confirmeRaw.length)
-        .map(Number)
-        .entries()) {
-        countryData.push({ date: date[i], confirmed: v });
+      const confirmeRaw = countryConfirmedRaw[idx].split(',');
+      const deathsRaw = countryDeathsRaw[idx].split(',');
+      const country = confirmeRaw[1];
+      for (let i = 4; i < confirmeRaw.length; i++) {
+        let relConfirmed = 0;
+        let relDeaths = 0;
+        if (i > 4) {
+          relConfirmed = this.calcRelative(
+            +confirmeRaw[i],
+            +confirmeRaw[i - 1]
+          );
+          relDeaths = this.calcRelative(+deathsRaw[i], +deathsRaw[i - 1]);
+        }
+        countryData.push({
+          date: date[i - 4],
+          confirmed: +confirmeRaw[i],
+          relConfirmed,
+          deaths: +deathsRaw[i],
+          relDeaths,
+        });
       }
-
-      if (confirmed.get(confirmeRaw[1]) === undefined) {
-        confirmed.set(confirmeRaw[1], countryData);
+      if (covidData.get(country) === undefined) {
+        covidData.set(country, countryData);
       } else {
-        confirmed.set(
-          confirmeRaw[1],
-          this.sumCountryData(confirmeRaw[1], confirmed, countryData)
+        covidData.set(
+          country,
+          this.sumCountryData(country, covidData, countryData)
         );
       }
     }
-    return confirmed;
+
+    return covidData;
   }
 
-  getCovidRawData(): Observable<any> {
+  getConfirmedCovidRawData(): Observable<any> {
+    const csvFile = 'time_series_covid19_confirmed_global.csv';
     const headers = new HttpHeaders().set(
       'Content-Type',
       'text/plain; charset=utf-8'
     );
-    return this.http.get(this.url, { headers, responseType: 'text' });
+    return this.http.get(this.urlGithubRaw + csvFile, {
+      headers,
+      responseType: 'text',
+    });
+  }
+
+  private calcRelative(cur, prev): number {
+    if (cur > 0) {
+      return +((cur - prev) / cur).toFixed(4);
+    }
+    return 0;
   }
 
   private sumCountryData(
     country: string,
     confirmed: Map<string, any[]>,
-    data: any[]
+    data: ICovidData[]
   ) {
     const sumData: any[] = [];
     for (const [i, v] of confirmed.get(country).entries()) {
+      let relConfirmed = 0;
+      let relDeaths = 0;
+
+      if (i > 0) {
+        relConfirmed = this.calcRelative(
+          v.confirmed + data[i].confirmed,
+          confirmed.get(country)[i - 1].confirmed + data[i - 1].confirmed
+        );
+        relDeaths = this.calcRelative(
+          v.deaths + data[i].deaths,
+          confirmed.get(country)[i - 1].deaths + data[i - 1].deaths
+        );
+      }
       sumData.push({
         date: v.date,
         confirmed: v.confirmed + data[i].confirmed,
+        deaths: v.deaths + data[i].deaths,
+        relConfirmed,
+        relDeaths,
       });
     }
     return sumData;
+  }
+
+  getDeathsCovidRawData(): Observable<any> {
+    const csvFile = 'time_series_covid19_deaths_global.csv';
+    const headers = new HttpHeaders().set(
+      'Content-Type',
+      'text/plain; charset=utf-8'
+    );
+    return this.http.get(this.urlGithubRaw + csvFile, {
+      headers,
+      responseType: 'text',
+    });
   }
 }
