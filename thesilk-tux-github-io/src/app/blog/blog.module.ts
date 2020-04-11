2494818ca38f3c614565c5bb7e297200854a0ada
theSilk <thesilk@posteo.net>
2020-04-11 22:45:21 +0200

    make difference between dashboard and app layout

--- thesilk-tux-github-io/src/app/blog/blog.module.ts
+++ thesilk-tux-github-io/src/app/blog/blog.module.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import { GeneralModule } from '../general/general.module';
 import { BlogEntryComponent } from './blog-entry/blog-entry.component';
 import { BlogOverviewComponent } from './blog-overview/blog-overview.component';
 import { BlogService } from './blog.service';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [BlogEntryComponent, BlogOverviewComponent],
-  imports: [CommonModule, HttpClientModule,RouterModule],
+  imports: [CommonModule, GeneralModule, HttpClientModule, RouterModule],
   providers: [BlogService],
 })
 export class BlogModule {}
