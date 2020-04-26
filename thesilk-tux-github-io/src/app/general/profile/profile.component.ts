ab90424ca1d39a6680c806df667b5b6a1e4cd516
theSilk <thesilk@posteo.net>
2020-04-26 23:20:02 +0200

    add twitter follow me button to profile

--- thesilk-tux-github-io/src/app/general/profile/profile.component.ts
+++ thesilk-tux-github-io/src/app/general/profile/profile.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    const url = 'https://platform.twitter.com/widgets.js';
+    if (!document.querySelector(`script[src='${url}']`)) {
+      const script = document.createElement('script');
+      script.src = url;
+      document.body.appendChild(script);
+    }
   }
 
+  ngOnInit(): void {}
 }
